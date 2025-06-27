8f4cfd466b8044dec3bb0c7ab75cee0bf7cf34fa
Joshua <adrishpinto423@gmail.com>
2025-06-27 17:46:08 +0530

    footer in product page

--- src/pages/ProductPage.jsx
+++ src/pages/ProductPage.jsx
@@ -6,6 +6,7 @@ import RevealScroll from "../components/ProductPage/RevealScroll";
 import Transition from "../components/ProductPage/Transition";
 import ProductShow from "../components/ProductPage/ProductShow";
 import BikeInfo from "../components/ProductPage/BikeInfo";
+import Footer from "../components/Common/Footer";
 
 const ProductPage = () => {
   return (
@@ -16,6 +17,7 @@ const ProductPage = () => {
       <Transition />
       <RevealScroll />
       <ProductShow />
+      <Footer />
     </div>
   );
 };
